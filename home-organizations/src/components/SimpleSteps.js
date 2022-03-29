0a20d429b090ba995bae7b395f97afe431e67201
Milena <milenachilinska@yahoo.com>
2022-03-29 20:31:22 +0200

    add icons on simplesSteps

--- home-organizations/src/components/SimpleSteps.js
+++ home-organizations/src/components/SimpleSteps.js
@@ -16,21 +16,25 @@ function SimpleSteps(props) {
                 <div className='simplestep-icons'>
                     <img src={tshirtImg} alt='icon' />
                     <h2>Wybierz rzeczy</h2>
+                    <hr></hr>
                     <p>Ubrania, zabawki, <br/> sprzet i inne</p>
                 </div>
                 <div className='simplestep-icons'>
                     <img src={bagImg} alt='icon' />
                     <h2>Spakuj je</h2>
+                    <hr></hr>
                     <p>Skorzystaj z <br/> workow na smieci</p>
                 </div>
                 <div className='simplestep-icons'>
                     <img src={magnifireImg} alt='icon' />
                     <h2>Zdecyduj komu <br /> chcesz pomoc</h2>
+                    <hr></hr>
                     <p>Wybierz zaufane <br/> miejsce</p>
                 </div>
                 <div className='simplestep-icons'>
                     <img src={arraysImg} alt='icon' />
                     <h2>Zamow kuriera</h2>
+                    <hr></hr>
                     <p>Kurier przyjedzie <br/> w dogodnym terminie</p>
                 </div>
             </div>
