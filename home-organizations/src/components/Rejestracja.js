77063ab2417545b27ef56454f53ae09e1bbb97f2
Milena <milenachilinska@yahoo.com>
2022-04-05 19:18:17 +0200

    add regestration

--- home-organizations/src/components/Rejestracja.js
+++ home-organizations/src/components/Rejestracja.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Link } from 'react-scroll';
+import { Link as RouterLink } from "react-router-dom";
+import decoration from '../assets/Decoration.svg';
 
 function Rejestracja(props) {
     return (
         <div>
-            
+            <nav>
+            <div className='header-menu'>
+            <ul>
+                <li><Link activeClass="home" to="home">Start</Link> </li>
+                <li><Link to="simpleSteps">O co chodzi?</Link> </li>
+                <li><Link to="aboutUs">O nas</Link> </li>
+                <li><Link to="helpInfo">Fundacja i organizacje</Link> </li>
+                <li><Link to="contact">Kontakt</Link> </li>
+            </ul>
+            </div>
+        </nav>
+        <div className='regestration-title'>
+            <h2>Załóż konto</h2>
+            <img src={decoration} alt='decoration' />
+        </div>
+        <div className='regestration-form'>
+            <h3>Email</h3>
+            <input type='email'/>
+            <h3>Hasło</h3>
+            <input type='password'/>
+            <h3>Powtorz hasło</h3>
+            <input type='password'/>
+        </div>
+        <div className='regestration-btns'>
+            <button>Zaloguj się</button>
+            <button><RouterLink to="/logowanie">Załóż konto</RouterLink></button>
+        </div>
         </div>
     );
 }
